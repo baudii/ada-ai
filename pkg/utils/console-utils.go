057972d38d3bcef8bc7f2bcddcef831b70d220d1
Baudi Kurbanov <screwyuguys@gmail.com>
2025-09-30 22:28:33 +0300

    fix: switched from using goto to for loop

--- pkg/utils/console-utils.go
+++ pkg/utils/console-utils.go
@@ -8,18 +8,19 @@ import (
 )
 
 func ReadInput(msg string) string {
-read:
-	fmt.Printf("%v > ", msg)
-	scanner := bufio.NewScanner(os.Stdin)
-	var line string
-	if scanner.Scan() {
-		line = scanner.Text()
-	}
+	for {
+		fmt.Printf("%v > ", msg)
+		scanner := bufio.NewScanner(os.Stdin)
+		var line string
+		if scanner.Scan() {
+			line = scanner.Text()
+		}
 
-	if err := scanner.Err(); err != nil {
-		slog.Error("error reading from stdin", "error", err)
-		goto read
-	}
+		if err := scanner.Err(); err != nil {
+			slog.Error("error reading from stdin", "error", err)
+			continue
+		}
 
-	return line
+		return line
+	}
 }
