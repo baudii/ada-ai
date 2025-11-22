fdb0bda51d4b9accfb97a6f2b8354c074ab46302
Baudi Kurbanov <screwyuguys@gmail.com>
2025-11-23 01:55:09 +0300

    feat(#57): added extract method that actually parses file and extracts types from it

--- internal/infra/openapiproject/codegenanalyzer.go
+++ internal/infra/openapiproject/codegenanalyzer.go
@@ -8,10 +8,12 @@ import (
 	"go/printer"
 	"go/types"
 	"log/slog"
+	"os"
 	"reflect"
 	"strings"
 	"unicode"
 
+	"github.com/baudii/ada-ai/internal/app/codeanalyzer"
 	"golang.org/x/tools/go/packages"
 )
 
@@ -57,6 +59,23 @@ func PascalToSnake(s string) string {
 	}
 
 	return string(out)
+}
+
+func Extract[T ast.Expr](path, filter string) (string, error) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return "", nil
+	}
+	f, err := os.ReadFile(path)
+	if err != nil {
+		return "", fmt.Errorf("read file: %w", err)
+	}
+
+	res, err := codeanalyzer.ExtractTypesOf[T](string(f), filter)
+	if err != nil {
+		return "", fmt.Errorf("extract type of %T: %w", *new(T), err)
+	}
+
+	return res, nil
 }
 
 func getMethodInfo(method *types.Func, pkg *packages.Package) MethodInfo {
